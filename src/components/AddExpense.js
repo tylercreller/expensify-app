d457bebae7fe66acd356025440aa340daa9f301c
Tyler Creller <tylercreller@gmail.com>
2019-04-28 08:16:21 -0400

    Setup form submit for adding an expense

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-const AddExpense = () => (
+const AddExpense = props => (
 	<React.Fragment>
 		<h1>Add Expense</h1>
-		<ExpenseForm />
+		<ExpenseForm
+			onSubmit={expense => {
+				props.dispatch(addExpense(expense));
+				props.history.push('/');
+			}}
+		/>
 	</React.Fragment>
 );
-export default AddExpense;
+export default connect()(AddExpense);
